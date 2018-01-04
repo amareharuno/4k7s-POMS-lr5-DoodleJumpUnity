93086ffd83b52b97a02622aceaf1b37e2eda33c9
Lyuba Eremeeva <leremeeva174@gmail.com>
2018-01-04 21:45:48 +0300

    second commit

---
+++ Doodle_Jump_unity/Doodle Jump/Assets/CameraFolow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFolow : MonoBehaviour {
+
+    public Transform target;
+    public GameObject gameOver;
+	
+	void LateUpdate () {
+        if (target.position.y > transform.position.y) {
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            transform.position = newPos;
+        }
+
+        if (transform.position.y - target.position.y > 5) {
+            Application.LoadLevel("GameOver");
+        }
+	}
+}
